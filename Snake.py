cb419616cb83bb91cd61cf2880f7d55d0b11f9cd
Brando <brandoflores16@gmail.com>
2020-12-16 20:04:41 -0700

    Add full snake movement. Prevent snake from moving backwards.

--- Snake.py
+++ Snake.py
@@ -2,14 +2,18 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
-
 
     def __init__(self):
         # Keep track of parts of snake
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     # Create the initial snake
     def create_snake(self):
@@ -27,4 +31,21 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
--- main.py
+++ main.py
@@ -12,6 +12,13 @@ screen.title("Snake")
 
 snake = Snake()
 
+# Key event listeners for the snake
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
 #Keep the game on
 while game_is_on:
